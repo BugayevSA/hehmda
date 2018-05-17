ce2f9654a62c80ad157faec973229f7068b08e12
u211-14 <u211-14@public.mpei.local>
2018-05-17 17:00:54 +0300

    nibble_to_hex_4

--- main.cpp
+++ main.cpp
@@ -50,6 +50,15 @@ void print_in_binary(const void* data, size_t size) {
         }
     }
 }
+struct Student{
+    char name[17];
+    uint32_t year;
+    float sred_ball;
+    int sex:1;
+    uint8_t courses;
+    Student* starosta;
+};
+
 int main () {
     assert(nibble_to_hex(0x0) == '0');
     assert(nibble_to_hex(0x1) == '1');
@@ -67,12 +76,31 @@ int main () {
     assert(nibble_to_hex(0xd) == 'd');
     assert(nibble_to_hex(0xe) == 'e');
     assert(nibble_to_hex(0xf) == 'f');
-//    uint32_t u32 = 0x42;
-//    cout << "u32 bytes: ";
-//    print_in_hex(&u32, sizeof(u32));
-//    cout << '\n';
-    print_in_hex(0xab);
-    cout <<"\n";
-    print_in_binary(3);
+    Student studarray[3]={
+            {"FIO1",2017,4.3,0,7,&studarray[1]},
+            {"FIO2",2018,5.4,1,6,nullptr},
+            {"FIO3",2019,6.5,0,5,&studarray[1]}
+    };
+    cout << "Adress pf array:" <<&studarray;
+    cout << "Size of array:" << sizeof (studarray);
+    cout << "\t Adress of elem: \t Size of elem: \n";
+    for (int i = 0; i<3; i++){
+        cout << i << ":\t" << &studarray[i] << "\t" << sizeof (studarray[i]) << "\n";
+    }
+    cout << "FIRST ELEMENT \n";
+    cout <<"\n"<<  "\t Adress of field :\t sizeof field \t offset :\n";
+    cout <<"\n"<<  "name:\t\t" << &studarray[0].name << "\t\t" << sizeof (studarray[0]) <<"\t\t"<<offsetof(struct Student,name);
+    cout <<"\n"<<  "year:\t\t" << &studarray[0].year << "\t\t" << sizeof (studarray[0]) <<"\t\t"<<offsetof(struct Student,year);
+    cout <<"\n"<<  "sred_ball:\t" << &studarray[0].sred_ball << "\t\t" << sizeof
+    (studarray[0]) <<"\t\t"<<offsetof(struct Student,sred_ball);
+    cout <<"\n"<< "courses:\t\t" << &studarray[0].courses << "\t\t" << sizeof
+    (studarray[0]) <<"\t\t"<<offsetof(struct Student,courses);
+    /* uint32_t u32 = 0x42;
+     cout << "u32 bytes: ";
+     print_in_hex(&u32, sizeof(u32));
+     cout << '\n'; */
+//    print_in_hex(0xab);
+//    cout <<"\n";
+//    print_in_binary(3);
     return 0;
 }

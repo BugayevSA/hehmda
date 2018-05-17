1075a930437b1e374b0bb7ff06c9e35753a0c86b
u211-14 <u211-14@public.mpei.local>
2018-05-17 15:21:33 +0300

    nibble_to_hex_2

--- main.cpp
+++ main.cpp
@@ -9,6 +9,47 @@ char digits[] = "0123456789abcdef";
 void print_in_hex (uint8_t byte){
 cout << nibble_to_hex(byte >> 4) << nibble_to_hex(byte & 0xf);
 }
+const uint8_t* as_bytes(const void* data ) {
+    return reinterpret_cast < const uint8_t*>(data);
+}
+void print_in_hex(const void* data, size_t size) {
+    const uint8_t* bytes = as_bytes(data);
+    for (size_t i = 0; i < size; i++) {
+        print_in_hex(bytes[i]);
+
+        if ((i + 1) % 16 == 0) {
+            cout << '\n';
+        }
+        else {
+            cout << ' ';
+        }
+    }
+}
+char
+bit_digit(uint8_t byte, uint8_t bit) {
+    if (byte & (0x1 << bit)) {
+        return '1';
+    }
+    return '0';
+}
+void print_in_binary(uint8_t byte) {
+    for (uint8_t bit = 7; bit > 0; bit--) {
+        cout << bit_digit(byte, bit);
+    }
+}
+void print_in_binary(const void* data, size_t size) {
+    const uint8_t* bytes = as_bytes(data);
+    for (size_t i = 0; i < size; i++) {
+        print_in_binary(bytes[i]);
+         
+        if ((i + 1) % 4 == 0) {
+            cout << '\n';
+        }
+        else {
+            cout << ' ';
+        }
+    }
+}
 int main () {
     uint8_t i;
     cin >> i;

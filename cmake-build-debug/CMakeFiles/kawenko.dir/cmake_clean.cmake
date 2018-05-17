ec478994e6452936fbe374ba62dfc73d4d26c7c2
u211-14 <u211-14@public.mpei.local>
2018-05-17 14:32:01 +0300

    nibble_to_hex

---
+++ cmake-build-debug/CMakeFiles/kawenko.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kawenko.dir/main.cpp.obj"
+  "kawenko.pdb"
+  "kawenko.exe"
+  "kawenko.exe.manifest"
+  "libkawenko.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kawenko.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
